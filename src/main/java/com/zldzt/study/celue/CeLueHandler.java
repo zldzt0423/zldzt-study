8ab3dfc8dbcf6bcefd3215f0542dd6af78c24fd5
zldzt <zhaolei@wezonet.com>
2023-07-21 10:12:38 +0800

    1.策略模式 2.责任链

--- src/main/java/com/zldzt/study/celue/CeLueHandler.java
+++ src/main/java/com/zldzt/study/celue/CeLueHandler.java
@@ -2,6 +2,10 @@ package com.zldzt.study.celue;
 
 import com.zldzt.study.celue.bean.Zldzt;
 
+/**
+ * 策略模式主要是优化if else写法方便后期维护
+ * 真要较真性能，还是if else性能更好
+ */
 public interface CeLueHandler {
     Boolean BeUse(Integer type);
 
